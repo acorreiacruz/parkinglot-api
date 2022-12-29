e30d197bb88d403a5ae29500913a9b7e74d70b4a
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-29 12:58:57 -0300

    Testes do caso de uso GetParkingLot.

---
+++ tests/usecases/memory/GetParkingLotTest.test.ts
@@ -0,0 +1,29 @@
+import ParkedCarMemoryRepository from "../../../src/infra/repositories/ParkedCarMemoryRepository";
+import ParkingLotMemoryRepository from "../../../src/infra/repositories/ParkingLotMemoryRepository";
+import GetParkingLot from "../../../src/usecases/GetParkingLot";
+
+let getParkingLot: GetParkingLot;
+
+beforeEach(async () => {
+    const parkedCarRepository = new ParkedCarMemoryRepository()
+    const parkingLotRepository = new ParkingLotMemoryRepository(parkedCarRepository);
+    await parkingLotRepository.saveParkingLot("Movie Theater", 35, 7, 22);
+    getParkingLot = new GetParkingLot(parkingLotRepository);
+});
+
+describe("Test GetParkingLot", () => {
+    test("Test if GetParkingLot return a parking lot", async () =>{
+        const parkingLot = await getParkingLot.execute("Movie Theater");
+        expect(parkingLot.code).toBe("Movie Theater");
+        expect(parkingLot.getCapacity()).toBe(35);
+        expect(parkingLot.getOccupiedSpaces()).toBe(0);
+        expect(parkingLot.openHour).toBe(7);
+        expect(parkingLot.closeHour).toBe(22);
+    });
+
+    test("Test if GetParkingLot throws a error when there isn't the parking lot", async () =>{
+        expect(
+            async () => await getParkingLot.execute("Super Market")
+        ).rejects.toThrowError("There is no ParkingLot with this code");
+    });
+});

b29e6fcbd0904190c45968dc1081628ee46881f5
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 00:22:43 -0300

    Criando o caso de uso ParkCar.

--- src/usecases/ParkCar.ts
+++ src/usecases/ParkCar.ts
@@ -1,3 +1,4 @@
+import ParkedCar from "../entities/ParkedCar";
 import ParkedCarRepository from "../infra/repositories/ParkedCarRepository";
 import ParkingLotRepository from "../infra/repositories/ParkingLotRepository";
 
@@ -7,8 +8,11 @@ export default class ParkCar {
         private parkingLotRepository: ParkingLotRepository
     ) { }
     async execute(code: string, plate: string, enterDate: string): Promise<void> {
+        if(!ParkedCar.isValid(plate)) throw new Error("This plate format shloud be XXXX000 or XXX0X00");
         const parkingLot = await this.parkingLotRepository.getParkingLot(code);
         if(!parkingLot) throw new Error("There is no parking lot with this code");
+        const parkedCar = await this.parkedCarRepository.getParkedCar(code, plate);
+        if(parkedCar) throw new Error("This car already is parked");
         if(!parkingLot.isOpen(new Date(enterDate))) throw new Error("The parking lot is closed");
         if(parkingLot.isFull()) throw new Error("The parking lot is full");
         await this.parkedCarRepository.saveParkedCar(code, plate, enterDate);

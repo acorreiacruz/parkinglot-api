d40b1d70c3365c020bdb8aec6d099694b1c8a604
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 00:27:29 -0300

    Alterando o retorno do getAllParkedCars para Promise<ParkedCar[]|undefined>.

--- src/infra/repositories/ParkedCarRepository.ts
+++ src/infra/repositories/ParkedCarRepository.ts
@@ -2,7 +2,7 @@ import ParkedCar from "../../entities/ParkedCar";
 
 export default interface ParkedCarRepository {
     saveParkedCar(code: string , plate: string, enterDate: string): Promise<void>;
-    getAllParkedCars(code: string): Promise<ParkedCar[]>;
+    getAllParkedCars(code: string): Promise<ParkedCar[]|undefined>;
     checkoutParkedCar(code: string, plate: string): Promise<void>;
     getOccupiedSpaces(code: string): Promise<number>;
     getParkedCar(code:string, plate:string): Promise<ParkedCar|undefined>;

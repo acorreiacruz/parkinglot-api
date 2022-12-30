0b55c76b81bc365b6e1cd9348697485cb4351ade
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 00:05:48 -0300

    Implementando getParkedCar no ParkedCarMemoryRepository.

--- src/infra/repositories/ParkedCarMemoryRepository.ts
+++ src/infra/repositories/ParkedCarMemoryRepository.ts
@@ -15,19 +15,31 @@ export default class ParkedCarMemoryRepository implements ParkedCarRepository {
         this.parkedCarsData = [];
     }
 
+    async getParkedCar(code: string, plate: string): Promise<ParkedCar> {
+        const parkedCarData = this.parkedCarsData.find(
+            (data) => data.code === code && data.plate === plate && !data.checkoutDate
+        );
+        if (!parkedCarData) throw new Error("There is no parked car with this informations");
+        return new ParkedCar(
+            parkedCarData.code,
+            parkedCarData.plate,
+            new Date(parkedCarData.enterDate)
+        );
+    }
+
     async saveParkedCar(code: string, plate: string, enterDate: string): Promise<void> {
         this.parkedCarsData.push({
-            code:code,
-            plate:plate,
-            enterDate:enterDate,
+            code: code,
+            plate: plate,
+            enterDate: enterDate,
             checkoutDate: ""
         })
     }
 
     async getAllParkedCars(code: string): Promise<ParkedCar[]> {
         const parkedCars: ParkedCar[] = [];
-        const parkedCarsData = this.parkedCarsData.filter((data) => data.code===code && !data.checkoutDate);
-        for(const data of parkedCarsData){
+        const parkedCarsData = this.parkedCarsData.filter((data) => data.code === code && !data.checkoutDate);
+        for (const data of parkedCarsData) {
             parkedCars.push(
                 new ParkedCar(data.code, data.plate, new Date(data.enterDate))
             );
@@ -36,21 +48,21 @@ export default class ParkedCarMemoryRepository implements ParkedCarRepository {
     }
 
     async checkoutParkedCar(code: string, plate: string): Promise<void> {
-        for(const data of this.parkedCarsData){
-            if(data.code === code  && data.plate == plate && !data.checkoutDate){
+        for (const data of this.parkedCarsData) {
+            if (data.code === code && data.plate == plate && !data.checkoutDate) {
                 data.checkoutDate = new Date().toISOString();
                 break;
             }
-            if(data.code === code  && data.plate == plate && data.checkoutDate){
+            if (data.code === code && data.plate == plate && data.checkoutDate) {
                 throw new Error("This car has already left the parking lot");
             }
         }
     }
 
     async getOccupiedSpaces(code: string): Promise<number> {
-        let total  = 0;
-        for(const data of this.parkedCarsData){
-            if(data.code === code && !data.checkoutDate) total +=1;
+        let total = 0;
+        for (const data of this.parkedCarsData) {
+            if (data.code === code && !data.checkoutDate) total += 1;
         }
         return total;
     }

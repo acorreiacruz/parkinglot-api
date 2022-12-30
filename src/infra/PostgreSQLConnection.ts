d2371bad58c826f9bb4b8ea7bc56043bba4b7660
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 13:36:40 -0300

    Implementando PostgreSQLConnection a partir da interface Connection.

---
+++ src/infra/PostgreSQLConnection.ts
@@ -0,0 +1,22 @@
+import Connection from "./Connection";
+import pgp from "pg-promise";
+
+export default class PostgeSQLConnection implements Connection {
+    private connection: any;
+    
+    constructor(){
+        this.connection = pgp()("postgres://super:super12345@localhost:54320/cleanarch");
+    }
+
+    query(statement: string, paramas: any[]): Promise<any> {
+        return this.connection.query(statement, paramas);
+    }
+
+    one(statement: string, paramas: any[]): Promise<any> {
+        return this.connection.oneOrNone(statement, paramas);
+    }
+
+    close(): Promise<void> {
+        return this.connection.$pool.end();
+    }
+}

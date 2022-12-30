3d597e335374c4f66fd5e4ad28ef790ef551b14a
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 20:11:49 -0300

    Removendo o método getOccupiedSpaces da interface ParkedCarRepository.

--- src/infra/repositories/ParkedCarRepository.ts
+++ src/infra/repositories/ParkedCarRepository.ts
@@ -4,6 +4,5 @@ export default interface ParkedCarRepository {
     saveParkedCar(code: string , plate: string, enterDate: string): Promise<void>;
     getAllParkedCars(code: string): Promise<ParkedCar[]|undefined>;
     checkoutParkedCar(code: string, plate: string): Promise<void>;
-    getOccupiedSpaces(code: string): Promise<number>;
     getParkedCar(code:string, plate:string): Promise<ParkedCar|undefined>;
 }

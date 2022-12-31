f32c7d948f888db5590a4ad60a2be65fe845f505
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-12-30 21:27:38 -0300

    Testes do caso d euso GetParkingLotParkedCars utilizando o db.

---
+++ tests/usecases/database/GetParkingLotParkedCarsTest.test.ts
@@ -0,0 +1,37 @@
+import PostgreSQLConnection from "../../../src/infra/PostgreSQLConnection";
+import ParkedCarSQLRepository from "../../../src/infra/repositories/ParkedCarSQLRepository";
+import ParkingLotSQLRepository from "../../../src/infra/repositories/ParkingLotSQLRepository";
+import GetParkingLotParkedCars from "../../../src/usecases/GetParkingLotParkedCars";
+
+let postgreSQLConnection: PostgreSQLConnection;
+let parkedCarRepository: ParkedCarSQLRepository;
+let parkingLotRepository: ParkingLotSQLRepository;
+let getParkingLotParkedCars: GetParkingLotParkedCars;
+
+beforeEach(async () => {
+    postgreSQLConnection = new PostgreSQLConnection()
+    parkedCarRepository = new ParkedCarSQLRepository(postgreSQLConnection);
+    parkingLotRepository = new ParkingLotSQLRepository(postgreSQLConnection);
+    getParkingLotParkedCars = new GetParkingLotParkedCars(parkedCarRepository, parkingLotRepository);
+    await parkingLotRepository.saveParkingLot("Orlando Outlet", 10, 7, 23);
+});
+
+describe("Test GetParkingLotParkedCars", () => {
+    test("Test if GetParkingLotParkerdCars returns all parked cars in parking lot", async () => {
+        await parkedCarRepository.saveParkedCar("Orlando Outlet", "FHG1B23", "2022-12-28T19:30:00");
+        await parkedCarRepository.saveParkedCar("Orlando Outlet", "FHG1B24", "2022-12-28T19:33:00");
+        await parkedCarRepository.saveParkedCar("Orlando Outlet", "FHG1B26", "2022-12-28T19:38:00");
+        await parkedCarRepository.saveParkedCar("Orlando Outlet", "FHG1B27", "2022-12-28T19:39:00");
+        const parkedCars = await getParkingLotParkedCars.execute("Orlando Outlet");
+        expect(parkedCars).toHaveLength(4);
+    });
+
+    test("Test if GetParkingLotParkerdCars throw error when there is no parked car", async () => {
+        expect(async () => await getParkingLotParkedCars.execute("Orlando Outlet")).rejects.toThrowError("There is no parked car in this parking lot");
+    });
+});
+
+afterEach(async () => {
+    await postgreSQLConnection.query("DELETE FROM project.parkinglot WHERE code=$1",["Orlando Outlet"]);
+    await postgreSQLConnection.close();
+});

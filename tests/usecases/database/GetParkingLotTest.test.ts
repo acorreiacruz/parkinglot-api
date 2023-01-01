5fed4df9410144e6b4ec032530221747705f8967
acorreiacruz <antoniocorreiacruz@outlook.com>
2023-01-01 15:09:06 -0300

    refactoring(GetParkingLotTest): usando o saveParkingLot como fixture do teste.

--- tests/usecases/database/GetParkingLotTest.test.ts
+++ tests/usecases/database/GetParkingLotTest.test.ts
@@ -9,6 +9,7 @@ let parkingLotRepository: ParkingLotSQLRepository;
 beforeEach(async () => {
     postgreSQLConnection = new PostgreSQLConnection();
     parkingLotRepository = new ParkingLotSQLRepository(postgreSQLConnection);
+    await parkingLotRepository.saveParkingLot("Burguer King", 10, 6, 24);
     getParkingLot = new GetParkingLot(parkingLotRepository);
 });
 
@@ -17,18 +18,17 @@ describe("Test GetParkingLot", () => {
         const parkingLot = await getParkingLot.execute("Burguer King");
         expect(parkingLot.code).toBe("Burguer King");
         expect(parkingLot.getCapacity()).toBe(10);
-        expect(parkingLot.getOccupiedSpaces()).toBe(3);
+        expect(parkingLot.getOccupiedSpaces()).toBe(0);
         expect(parkingLot.openHour).toBe(6);
         expect(parkingLot.closeHour).toBe(24);
     });
 
-    test("Test if GetParkingLot throws a error when there isn't the parking lot", async () => {
         expect(
             async () => await getParkingLot.execute("Jhon Hopkins Hospital")
-        ).rejects.toThrowError("There is no ParkingLot with this code");
     });
 });
 
 afterEach(async () => {
+    await postgreSQLConnection.query("DELETE FROM parkinglotca.parkinglot WHERE code = $1", ["Burguer King"]);
     await postgreSQLConnection.close();
 })

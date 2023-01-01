1c443f968c92eb8c17f4dd30b14151309fe9b169
acorreiacruz <antoniocorreiacruz@outlook.com>
2023-01-01 11:48:01 -0300

    refactor(ParkCar): implementando o erro ParkingLotDoesNotExists.

--- src/usecases/ParkCar.ts
+++ src/usecases/ParkCar.ts
@@ -1,3 +1,5 @@
+import InvalidCarPlate from "../entities/errors/InvalidCarPlate";
+import ParkingLotDoesNotExists from "../entities/errors/ParkingLotDoesNotExists";
 import ParkedCar from "../entities/ParkedCar";
 import ParkedCarRepository from "../infra/repositories/ParkedCarRepository";
 import ParkingLotRepository from "../infra/repositories/ParkingLotRepository";
@@ -8,9 +10,9 @@ export default class ParkCar {
         private parkingLotRepository: ParkingLotRepository
     ) { }
     async execute(code: string, plate: string, enterDate: string): Promise<void> {
-        if(!ParkedCar.isValid(plate)) throw new Error("This plate format shloud be XXXX000 or XXX0X00");
+        if(!ParkedCar.isValid(plate)) throw new InvalidCarPlate();
         const parkingLot = await this.parkingLotRepository.getParkingLot(code);
-        if(!parkingLot) throw new Error("There is no parking lot with this code");
+        if(!parkingLot) throw new ParkingLotDoesNotExists();
         const parkedCar = await this.parkedCarRepository.getParkedCar(code, plate);
         if(parkedCar) throw new Error("This car already is parked");
         if(!parkingLot.isOpen(new Date(enterDate))) throw new Error("The parking lot is closed");

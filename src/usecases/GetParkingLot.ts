8d94b05d3deb7b1c1d67759f0f38dadc79bfa2d5
acorreiacruz <antoniocorreiacruz@outlook.com>
2023-01-01 12:10:24 -0300

    refactor(GetParkingLot): implementando o erro ParkingLotDoesNotExists.

--- src/usecases/GetParkingLot.ts
+++ src/usecases/GetParkingLot.ts
@@ -1,3 +1,4 @@
+import ParkingLotDoesNotExists from "../entities/errors/ParkingLotDoesNotExists";
 import ParkingLot from "../entities/ParkingLot";
 import ParkingLotRepository from "../infra/repositories/ParkingLotRepository";
 
@@ -5,7 +6,7 @@ export default class GetParkingLot {
     constructor(private parkingLotRepository: ParkingLotRepository){}
     async execute(code: string): Promise<ParkingLot>{
         const parkingLot = await this.parkingLotRepository.getParkingLot(code);
-        if(! parkingLot) throw new Error("There is no ParkingLot with this code");
+        if(! parkingLot) throw new ParkingLotDoesNotExists();
         return parkingLot;
     }
 }

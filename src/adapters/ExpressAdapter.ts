d0fd459bf55a7b6e222e02cdd89c1c3d96d99a25
acorreiacruz <antoniocorreiacruz@outlook.com>
2023-01-01 22:37:35 -0300

    refactor(ExpressAdapter.create):definindo ControllerOutput com saída.

--- src/adapters/ExpressAdapter.ts
+++ src/adapters/ExpressAdapter.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { ControllerOutput } from "../controller/Controller";
 
 export default class ExpressAdapter {
     static create(fn: Function) {
         return async (request: Request, response: Response) => {
-            const object = await fn(request.params, request.body);
-            return response.json(object);
+            const { body, statusCode }: ControllerOutput = await fn(request.params, request.body);
+            return response.status(statusCode).json(body);
         }
     }
 }
+
